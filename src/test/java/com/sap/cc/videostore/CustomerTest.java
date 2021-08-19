0bec11d36b4bbda25710856c13854a2e32eb9b4d
C5309084 <sueleyman.bilgi@sap.com>
2021-08-19 11:04:34 +0200

    refactor CustomerTest - extract movie variables to fields - move initialization of those to setup method

--- src/test/java/com/sap/cc/videostore/CustomerTest.java
+++ src/test/java/com/sap/cc/videostore/CustomerTest.java
@@ -1,10 +1,22 @@
 package com.sap.cc.videostore;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
+
+    private Movie regularMovie;
+    private Movie newReleaseMovie;
+    private Movie childrensMovie;
+
+    @BeforeEach
+    void setUp() {
+        regularMovie = new Movie("RRRrrrr!!!", Movie.REGULAR);
+        newReleaseMovie = new Movie("Interstellar", Movie.NEW_RELEASE);
+        childrensMovie = new Movie("Zootopie", Movie.CHILDRENS);
+    }
 
     @Test
     public void testStatementNoRental() {
@@ -17,7 +29,7 @@ public class CustomerTest {
     @Test
     public void testStatementRegularShortRental() {
         Customer customer = new Customer("Me");
-        customer.addRental(new Rental(new Movie("RRRrrrr!!!", Movie.REGULAR), 2));
+        customer.addRental(new Rental(regularMovie, 2));
         assertEquals("Rental Record for Me\n" +
                 "\tRRRrrrr!!!\t2.0\n" +
                 "Amount owed is 2.0\n" +
@@ -27,7 +39,7 @@ public class CustomerTest {
     @Test
     public void testStatementRegularLongRental() {
         Customer customer = new Customer("Me");
-        customer.addRental(new Rental(new Movie("RRRrrrr!!!", Movie.REGULAR), 10));
+        customer.addRental(new Rental(regularMovie, 10));
         assertEquals("Rental Record for Me\n" +
                 "\tRRRrrrr!!!\t14.0\n" +
                 "Amount owed is 14.0\n" +
@@ -37,7 +49,7 @@ public class CustomerTest {
     @Test
     public void testStatementNewReleaseShortRental() {
         Customer customer = new Customer("Me");
-        customer.addRental(new Rental(new Movie("Interstellar", Movie.NEW_RELEASE), 1));
+        customer.addRental(new Rental(newReleaseMovie, 1));
         assertEquals("Rental Record for Me\n" +
                 "\tInterstellar\t3.0\n" +
                 "Amount owed is 3.0\n" +
@@ -47,7 +59,7 @@ public class CustomerTest {
     @Test
     public void testStatementNewReleaseLongRental() {
         Customer customer = new Customer("Me");
-        customer.addRental(new Rental(new Movie("Interstellar", Movie.NEW_RELEASE), 3));
+        customer.addRental(new Rental(newReleaseMovie, 3));
         assertEquals("Rental Record for Me\n" +
                 "\tInterstellar\t9.0\n" +
                 "Amount owed is 9.0\n" +
@@ -57,7 +69,7 @@ public class CustomerTest {
     @Test
     public void testStatementChildrensShortRental() {
         Customer customer = new Customer("Me");
-        customer.addRental(new Rental(new Movie("Zootopie", Movie.CHILDRENS), 2));
+        customer.addRental(new Rental(childrensMovie, 2));
         assertEquals("Rental Record for Me\n" +
                 "\tZootopie\t1.5\n" +
                 "Amount owed is 1.5\n" +
@@ -67,7 +79,7 @@ public class CustomerTest {
     @Test
     public void testStatementChildrensLongRental() {
         Customer customer = new Customer("Me");
-        customer.addRental(new Rental(new Movie("Zootopie", Movie.CHILDRENS), 6));
+        customer.addRental(new Rental(childrensMovie, 6));
         assertEquals("Rental Record for Me\n" +
                 "\tZootopie\t6.0\n" +
                 "Amount owed is 6.0\n" +
@@ -77,9 +89,9 @@ public class CustomerTest {
     @Test
     public void testStatementSeveralRentals() {
         Customer customer = new Customer("Me");
-        customer.addRental(new Rental(new Movie("Zootopie", Movie.CHILDRENS), 1));
-        customer.addRental(new Rental(new Movie("Interstellar", Movie.NEW_RELEASE), 2));
-        customer.addRental(new Rental(new Movie("RRRrrrr!!!", Movie.REGULAR), 1));
+        customer.addRental(new Rental(childrensMovie, 1));
+        customer.addRental(new Rental(newReleaseMovie, 2));
+        customer.addRental(new Rental(regularMovie, 1));
         assertEquals("Rental Record for Me\n" +
                 "\tZootopie\t1.5\n" +
                 "\tInterstellar\t6.0\n" +
